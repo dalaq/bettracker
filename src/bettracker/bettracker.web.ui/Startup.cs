40860f9167d08ec22d87f1e295624c3bc95ec59a
Steve Grainger <steve.m.grainger@gmail.com>
2015-01-24 23:14:35 +0000

    Add initial solution, asp.net mvc website and test project

---
+++ src/bettracker/bettracker.web.ui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bettracker.web.ui.Startup))]
+namespace bettracker.web.ui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
